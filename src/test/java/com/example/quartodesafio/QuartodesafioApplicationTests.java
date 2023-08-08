04721f24496c101509f33f2e6bffa4b73f35b383
Rafael Ch <rafael.cherman0@gmail.com>
2023-08-08 20:23:38 -0300

    primeiro commit

---
+++ src/test/java/com/example/quartodesafio/QuartodesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quartodesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartodesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
